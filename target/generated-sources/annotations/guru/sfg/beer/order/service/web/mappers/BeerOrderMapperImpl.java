fb8c9babd2cc234dca3af650c1c199d47db45adb
Michael Blasentein <mblasen@gmail.com>
2019-09-27 19:20:39 -0700

    Refactored POM to use BOM at Maven Central

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-09-27T10:09:16-0700",
+    date = "2019-09-27T19:18:33-0700",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.4 (Oracle Corporation)"
 )
 @Component
